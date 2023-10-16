76598c9dfe48f2581360042ee446a5ec4b06bfde
Adam Jałocha <jalochadev@gmail.com>
2023-10-16 18:09:53 +0200

    Added react-redux, @reduxjs/toolkit to dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./app.module.scss";
+import { RootState } from "./store";
+import { increment } from "./features/counter/counterSlice";
 
 function App() {
-  return <div className={styles.app}>TODO LIST</div>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className={styles.app}>
+      {count}
+      <button onClick={() => dispatch(increment())}>Increment</button>
+    </div>
+  );
 }
 
 export default App;

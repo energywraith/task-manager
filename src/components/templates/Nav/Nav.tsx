bd3df79da2011331724a5461255f27964dba2a12
Adam Jałocha <jalochadev@gmail.com>
2023-10-17 20:27:22 +0200

    Added Nav Component

---
+++ src/components/templates/Nav/Nav.tsx
@@ -0,0 +1,21 @@
+import { Button, Container } from "components/common";
+import styles from "./styles.module.scss";
+import { GithubIcon } from "components/icons/GithubIcon";
+
+const Nav = () => {
+  return (
+    <nav className={styles.nav}>
+      <Container className={styles["nav__container"]}>
+        <h1>Todo list</h1>
+        <a
+          className={styles["nav__link"]}
+          href="https://github.com/energywraith/todo-list"
+        >
+          <Button className={styles["nav__button"]} Icon={GithubIcon} />
+        </a>
+      </Container>
+    </nav>
+  );
+};
+
+export { Nav };

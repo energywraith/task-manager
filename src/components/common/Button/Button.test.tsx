3a464ce6bedc63253a12abac4c1c98a878028ce8
Adam Jałocha <jalochadev@gmail.com>
2023-10-18 12:01:06 +0200

    Added Button unit tests

---
+++ src/components/common/Button/Button.test.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Button } from "./Button";
+import { DragIcon } from "components/icons/DragIcon";
+
+test("Button should have TextContent equal to Children", async () => {
+  const mockTextContent = "Children";
+
+  render(<Button>{mockTextContent}</Button>);
+
+  const button = await screen.findByTestId("button");
+  expect(button).toHaveTextContent(mockTextContent);
+});
+
+test("Button should render Icon when passed", async () => {
+  render(<Button Icon={DragIcon} />);
+
+  const button = await screen.findByTestId("button");
+  const icon = await screen.findByTestId("button-icon");
+
+  expect(button).toContainElement(icon);
+});
+
+test("Button should execute onClick when clicked", async () => {
+  const mockOnClick = jest.fn();
+
+  render(<Button onClick={mockOnClick}>Children</Button>);
+
+  const button = await screen.findByTestId("button");
+  expect(button).toBeInTheDocument();
+
+  fireEvent.click(button);
+  expect(mockOnClick).toBeCalled();
+});
--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -7,8 +7,14 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button = ({ children, Icon, className, ...props }: ButtonProps) => {
   return (
-    <button {...props} className={`${styles.button} ${className}`}>
-      {Icon && <Icon className={styles["button__icon"]} />}
+    <button
+      {...props}
+      className={`${styles.button} ${className}`}
+      data-testid="button"
+    >
+      {Icon && (
+        <Icon className={styles["button__icon"]} data-testid="button-icon" />
+      )}
       {children}
     </button>
   );
